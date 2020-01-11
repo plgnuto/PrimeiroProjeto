e954ec360d272c34ed5f641a10f0f79e670e2fcf
Pedro Galastri <plgnuto@gmail.com>
2020-01-10 23:42:07 -0300

    Revertando estado da primeira classe

--- src/br/com/bertini/primeiroprojeto/PrimeiraClasseJava.java
+++ src/br/com/bertini/primeiroprojeto/PrimeiraClasseJava.java
@@ -1,17 +1,11 @@
 package br.com.bertini.primeiroprojeto;
 
-import br.com.bertini.primeiroprojeto.dominio.Cliente;
-
 public class PrimeiraClasseJava {
 
 	
 	public static void main(String[] args) {
 
-		Cliente cliente = new Cliente();
-		
-		System.out.println(cliente.getIdadePrimitivo());
-		System.out.println(cliente.getIdadeWrapper());
-		
+		System.out.println("Primeira CLasse Java!");
 	}
 
 	
